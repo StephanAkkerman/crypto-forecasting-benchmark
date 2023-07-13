7414e11a18f30a421a2ee77925aa0d1d28ab442b
Akkie100 <stephan.akkerman@gmail.com>
2023-07-13 12:51:40 +0200

    Create main_tbats.py

---
+++ src/main_tbats.py
@@ -0,0 +1,19 @@
+import torch
+import os
+
+from experiment import forecast
+
+if __name__ == "__main__":
+    # Check the number of GPUs available
+    num_gpus = torch.cuda.device_count()
+
+    # Get the name of each GPU
+    devices = [torch.cuda.get_device_name(i) for i in range(num_gpus)]
+
+    # Print the devices
+    for i, device in enumerate(devices):
+        print(f"Device {i}: {device}")
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # Use only the first GPU.
+
+    forecast.forecast_model("TBATS")
