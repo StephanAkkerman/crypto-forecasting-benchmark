a7dd7ef041504a0a04dae0b317219769e3e809f7
Akkie100 <stephan.akkerman@gmail.com>
2023-08-07 09:52:29 +0200

    added code to generate missing forecasts

--- src/main.py
+++ src/main.py
@@ -49,8 +49,13 @@ def methods():
 if __name__ == "__main__":
     # volatility.plotly_volatility("15m")
     # forecast.raw_forecast_model("Prophet")
-    # forecast.find_missing_forecasts("raw_models")
+    # forecast.find_missing_forecasts(
+    #    "extended_models", ["NBEATS", "RNN", "LSTM", "GRU", "TCN", "TFT", "NHiTS"]
+    # )
     forecast.create_missing_forecasts("raw_models")
+    forecast.create_missing_forecasts(
+        "extended_models", ["NBEATS", "RNN", "LSTM", "GRU", "TCN", "TFT", "NHiTS"]
+    )
     # forecast.raw_all(ignore_model=["TBATS", "Prophet"])
 
     # forecast.forecast_all(ignore_model=["ARIMA", "TBATS", "Prophet"])
